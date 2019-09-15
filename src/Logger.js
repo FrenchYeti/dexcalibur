1458f54d3b28fc7f5d6e426767930ce9012dedb5
FrenchYeti <georges.michel@protonmail.com>
2019-09-15 17:15:26 +0200

    Add test mode to Logger behavior

--- src/Logger.js
+++ src/Logger.js
@@ -2,6 +2,21 @@ const Chalk = require("chalk");
 const Process = require("process"); 
 
 
+
+var callbacks = {
+    exit: function(){
+        Process.exit();
+    }
+};
+
+
+var flush = function(){
+    let s = prefix.join("");
+    prefix = [];
+    return s;
+}
+
+
 function multi_concat(msg){
     let ret="";
     for(let i=0; i<msg.length; i++)
@@ -10,20 +25,123 @@ function multi_concat(msg){
     return ret;
 }
 
-var callbacks = {
-    exit: function(){
-        Process.exit();
-    }
-};
-
 var prefix = [];
 
 
-var flush = function(){
-    let s = prefix.join("");
-    prefix = [];
-    return s;
+class TestLogger
+{
+    constructor(){
+        this.prefix = [];
+        this.cache = [];
+        this.cacheTag = null;
+    }
+
+    error(){
+        this.cache.push({ type:"error", val:multi_concat(arguments) });
+        return callbacks;
+    }
+
+    debug(){
+        this.cache.push({ type:"debug", val:multi_concat(arguments) });
+        return callbacks;
+    }
+
+    info2(){
+        this.cache.push({ type:"info", val:multi_concat(arguments) });
+        return callbacks;
+    }
+
+    info(){
+        this.cache.push({ type:"info", val:multi_concat(arguments) });
+        return callbacks;
+    }
+
+    setTagCache(tag){
+        this.cacheTag = tag;
+    }
+
+    removeTagCache(tag){
+        this.cacheTag = null;
+    }
+
+    expect(cache, fn=null){
+        let f = false;
+        this.cacheTag.map(x => {
+            if(x.type==cache.type){
+                if(f!=null)
+                    f = fn(cache, x);
+                else
+                    f = (x.val===cache.val);
+            }
+        });
+        return f;
+    }
+
+    clearCache(){
+        this.cache = [];
+    }
+
+    pop(){
+        return this.prefix.pop()
+    }
+
+    push(prefix){
+        this.prefix.push(prefix);
+        return this.prefix;
+    }
 }
+
+
+class ProdLogger
+{
+    constructor(){
+        this.prefix = [];
+
+    }
+
+    error(){
+        console.log(Chalk.bold.red('[ERROR] '+prefix.join("")+multi_concat(arguments)));
+        return callbacks;
+    }
+
+    debug(){
+        console.log(Chalk.bold.red('[DEBUG] '+prefix.join("")+multi_concat(arguments)));
+        return callbacks;
+    }
+
+    info2(){
+        console.log(Chalk.yellow('[INFO] '+prefix.join("")+multi_concat(arguments)));
+        return callbacks;
+    }
+
+    info(){
+        console.log(Chalk.yellow('[INFO] '+prefix.join("")+multi_concat(arguments)));
+        return callbacks;
+    }
+
+    pop(){
+        return this.prefix.pop()
+    }
+
+    push(prefix){
+        this.prefix.push(prefix);
+        return this.prefix;
+    }
+}
+
+class LoggerFactory
+{
+    static createLogger(testMode=false){
+        if(testMode)
+            return new TestLogger();
+        else
+            return new ProdLogger();
+    }
+}
+
+
+
+/*
 
 module.exports = {    
     error: function(msg){
@@ -50,4 +168,6 @@ module.exports = {
         return prefix;
     },
     flush: flush
-};
+};*/
+
+module.exports = LoggerFactory.createLogger; //()
